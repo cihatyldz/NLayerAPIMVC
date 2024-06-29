899f3eec5dc027c4b26e9e678dd2029b38fcc17c
cihat yıldız <cihatyldz1907@gmail.com>
2024-06-29 20:44:31 +0300

    mvc katmanı

---
+++ NLayer.Web/Views/Products/Save.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NLayer.Web.Views.Products
+{
+    public class SaveModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NLayer.Web/Views/Products/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NLayer.Web.Views.Products
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
